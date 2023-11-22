7114a73692088d8f9487ceaf3eba7ef5b21007f1
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-11-22 12:04:58 +0000

    use Dagger Secret for CHROMATIC_PROJECT_TOKEN

--- src/dagger/lib.ts
+++ src/dagger/lib.ts
@@ -1,4 +1,9 @@
-import Client, { Directory, DirectoryID } from "../../deps.ts";
+import Client, {
+  Directory,
+  DirectoryID,
+  Secret,
+  SecretID,
+} from "../../deps.ts";
 
 export const getDirectory = (
   client: Client,
@@ -10,4 +15,23 @@ export const getDirectory = (
     });
   }
   return src instanceof Directory ? src : client.host().directory(src);
+};
+
+export const getChromaticToken = (client: Client, token?: string | Secret) => {
+  if (Deno.env.get("CHROMATIC_PROJECT_TOKEN")) {
+    return client.setSecret(
+      "CHROMATIC_PROJECT_TOKEN",
+      Deno.env.get("CHROMATIC_PROJECT_TOKEN")!
+    );
+  }
+  if (token && typeof token === "string") {
+    if (token.startsWith("core.Secret")) {
+      return client.loadSecretFromID(token as SecretID);
+    }
+    return client.setSecret("CHROMATIC_PROJECT_TOKEN", token);
+  }
+  if (token && token instanceof Secret) {
+    return token;
+  }
+  return undefined;
 };
